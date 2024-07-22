60aa46960a4618150e9db5ab96acf511cf69049a
Zasmooncat <zasmomoxipol@gmail.com>
2024-07-22 11:57:30 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generate() {
+  let who = ["Mi gata", "El perro de mi amiga", "Un chaval", "Un yonki"];
+  let action = ["se comió", "se meó en", "aplastó", "destrozó"];
+  let what = ["mis apuntes", "mi telefono", "la moto"];
+  let when = [
+    "antes de salir",
+    "cuando estaba durmiendo",
+    "mientras estaba en el gym",
+    "cuando estaba comiendo",
+    "mientras hacía tai chi"
+  ];
+
+  let who1 = who[Math.floor(Math.random() * who.length)];
+  let action1 = action[Math.floor(Math.random() * action.length)];
+  let what1 = what[Math.floor(Math.random() * what.length)];
+  let when1 = when[Math.floor(Math.random() * when.length)];
+
+  const excusa = `${who1} ${action1} ${what1} ${when1}.`;
+
+  document.getElementById("excuse").innerHTML = excusa;
+}
+document.getElementById("generate").addEventListener("click", generate);
